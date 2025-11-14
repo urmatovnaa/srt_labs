4eea85c645d3f4121264789a83c378a1c7e56a7d
Altynai <urmatovnaa05@gmail.com>
2025-11-14 11:45:46 +0200

    add 2-lab

---
+++ lab2/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+// std::mutex m;
+
+void Func(std::string name) {
+    long double i = 0;
+    auto start = std::chrono::steady_clock::now();
+    
+    while (std::chrono::steady_clock::now() - start < std::chrono::seconds(1)) {
+        i += 1e-9L;  // 10^(-9)
+    }
+    
+    // m.lock();
+    std::cout << name;
+    std::cout << ": ";
+    std::cout << i;
+    std::cout << std::endl;   
+     // m.unlock();
+}
+
+int main() {
+    std::thread thread1(Func, "t1");
+    std::thread thread2(Func, "t2");
+    std::thread thread3(Func, "t3");
+    
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    
+    return 0;
+}
